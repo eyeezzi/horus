421d78475202b51726172eeb4b550494d7ee4ade
Uzziah Eyee <uzziah.eyee@shopify.com>
2018-08-24 22:15:13 -0230

    Created a basic express app that serves a token via an endpoint.

--- api-server/src/app.js
+++ api-server/src/app.js
@@ -1,1 +1,11 @@
 const app = require('express')()
+const uuid = require('uuid/v1')
+
+app.use('/api/v1/tokens', (req, res) => {
+	console.log(`Handling request for token`)
+	res.json({token: uuid()})
+})
+
+const server = app.listen(process.env.PORT, () => {
+	console.log(`Listening on ${ server.address().address }:${ server.address().port }`)
+})
